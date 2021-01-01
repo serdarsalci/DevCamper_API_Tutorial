7fd8e18afb42212b1c5f4f958e3e24a8a5f091ad
Serdar Salci <serdarsalci@gmail.com>
2020-12-31 20:08:31 -0500

    end of part 34

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
+const geocoder = require('../utils/geocoder');
 const asyncHandler = require('../middleware/async.js');
 
 // @description   Get all bootcamps
@@ -22,7 +23,18 @@ const asyncHandler = require('../middleware/async.js');
 
 // with the wrapper function
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-	const bootcamps = await Bootcamp.find();
+	let query;
+	let queryStr = JSON.stringify(req.query);
+
+	// Add $ sign in front of mongosse comparison query operator
+	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+	// parse it to object pass it to find
+	query = Bootcamp.find(JSON.parse(queryStr));
+	const bootcamps = await query;
+
+	//const bootcamps = await Bootcamp.find();
+	console.log(req.query);
 	res.status(200).json({
 		success: true,
 		count: bootcamps.length,
@@ -91,4 +103,31 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 		);
 	}
 	res.status(200).json({ success: true, data: {} });
+});
+
+// @description  Get Bootcamps within a radius
+// @route         GET/api/v1/bootcamps/radius/:zipcode/:distance
+// @access        Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	// Get lat/lng from geocoder
+	const loc = await geocoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
+
+	// Calc radius using radians
+	// Divide dist by radius of earth
+	// radius of Earth =  3,963 mi /  6,378km
+
+	const radius = distance / 3963;
+	const bootcamps = await Bootcamp.find({
+		location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+	});
+
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		data: bootcamps,
+	});
 });
