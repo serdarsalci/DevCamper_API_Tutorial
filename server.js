89275a453c9ce536f405144533ef7b054a6f58c8
Serdar Salci <serdarsalci@gmail.com>
2021-01-14 15:57:00 -0500

    end of part 69 and

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 const fileupload = require('express-fileupload');
 const path = require('path');
-const colors = require('colors');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const color = require('colors');
 const errorHandler = require('./middleware/error');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -41,8 +46,27 @@ app.use(fileupload());
 // Sanitize data
 app.use(mongoSanitize());
 
-// Set Static Folder
+// Set security headers
+app.use(helmet());
 
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // ten minutes
+	max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
